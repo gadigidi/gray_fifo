62a5f5832e0ac835a31fddb5b39c6556147bf39a
gadigidi <gadigidi@gmail.com>
2025-11-30 14:50:00 +0200

    Add files for EDAplayground

---
+++ design/design.sv
@@ -0,0 +1,9 @@
+// This file is needed for EDAplayground
+`include "decoders.sv"
+`include "synchronizer.sv"
+`include "wr_ctrl.sv"
+`include "rd_ctrl.sv"
+`include "fifo_mem.sv"
+`include "g_fifo_if.sv"
+`include "g_fifo.sv"
+
---
+++ tb/testbench.sv
@@ -0,0 +1,10 @@
+// This file is nedded for EDAplayground
+`include "generator.sv"
+`include "drivers.sv"
+`include "monitors.sv"
+`include "agents.sv"
+`include "scoreboard.sv"
+`include "env.sv"
+`include "test.sv"
+`include "tb.sv"
+
